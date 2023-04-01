890497463e35a432a6a7902de0ed2366becfd105
Elvis <elviscruz45@gmail.com>
2023-03-31 22:10:43 -0500

    solve the issues

--- conveyor-belt-indeler-app/src/components/AddInformation/AddInformation/ChangeDisplayBelt/Pickermode.js
+++ conveyor-belt-indeler-app/src/components/AddInformation/AddInformation/ChangeDisplayBelt/Pickermode.js
@@ -1,20 +1,5 @@
-import { View, Text, Platform, Picker } from "react-native";
+import { View, Text, Platform } from "react-native";
 import React, { useState } from "react";
-import DatePicker from "@dietime/react-native-date-picker";
 import { Input, Button } from "@rneui/themed";
 
-export function Pickermode() {
-  const [date, setDate] = useState();
-
-  return (
-    <View>
-      <Text>{date ? date.toDateString() : "Select date..."}</Text>
-      <DatePicker
-        value={0}
-        onChange={(value) => setDate(value)}
-        format="yyyy-mm-dd"
-        startYear={0}
-      />
-    </View>
-  );
-}
+export function Pickermode() {}
